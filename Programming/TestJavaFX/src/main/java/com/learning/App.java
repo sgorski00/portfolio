4ca8650fb47618b2f6b420f5f37642e9f9f17d52
Sebastian Górski <sebastiangorski00@gmail.com>
2023-07-11 12:46:09 +0200

    test v2

--- Programming/TestJavaFX/src/main/java/com/learning/App.java
+++ Programming/TestJavaFX/src/main/java/com/learning/App.java
@@ -1,5 +1,6 @@
 package com.learning;
 
+import com.learning.controller.StackPaneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,8 +20,9 @@ public class App extends Application {
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(this.getClass().getResource("/fxml/StackPaneWindow.fxml"));
-
         StackPane stackPane = loader.load();
+
+        StackPaneController controller = loader.getController();
 
         Scene scene = new Scene(stackPane);
         primaryStage.setScene(scene);
