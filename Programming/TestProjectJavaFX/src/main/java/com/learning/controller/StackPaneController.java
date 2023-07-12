6b8726ef497d3886d93c0060bde659b5430e6c8e
Sebastian Górski <sebastiangorski00@gmail.com>
2023-07-12 12:38:27 +0200

    First events in JavaFX.

--- Programming/TestProjectJavaFX/src/main/java/com/learning/controller/StackPaneController.java
+++ Programming/TestProjectJavaFX/src/main/java/com/learning/controller/StackPaneController.java
@@ -1,7 +1,10 @@
 package com.learning.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.MouseEvent;
 
 public class StackPaneController {
     @FXML
@@ -14,5 +17,15 @@ public class StackPaneController {
     @FXML
     void initialize(){
         button.setText("New button");
+    }
+
+    @FXML
+    public void onAction(ActionEvent event){
+        System.out.println("On action method. The source is: " + event.getSource());
+        if(event.getSource() instanceof CheckBox){
+            System.out.println("You pressed the checkbox.");
+        } else {
+            System.out.println("You pressed the button.");
+        }
     }
 }
